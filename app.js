7752cb8646f07bf2491f6c9bd9f59e7983c89972
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2023-06-15 04:16:56 +0600

    classes data get from the database and api created for classes

--- app.js
+++ app.js
@@ -40,6 +40,16 @@ async function run() {
             res.status(200).json({ message: "welcome to server" });
         });
 
+                // get all classes
+                app.get("/classes", async(req, res) =>{
+                    const allClasses = await classCollection.find().toArray();
+                    // console.log(allClasses);
+            
+                    allClasses.length > 0
+                        ? res.status(200).json(allClasses)
+                        : res.status(404).json({ error: "data not found" });
+                });
+
 
         app.use((req, res, next) => {
             res.status(404).json({
