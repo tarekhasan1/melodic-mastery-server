de696c463718c240244bea47e396db331433d2c7
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2023-06-17 23:25:01 +0600

    vercel.json added and .env file added for database security

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://melodicmastery:QgtVXwZo1kT3EXPU@cluster0.ro4rymx.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.DB_URL;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-     client.connect();
+    client.connect();
 
     // create a collection for user
     const userCollection = client.db("MelodicMasteryDB").collection("users");
@@ -90,55 +89,52 @@ async function run() {
         : res.status(400).json({ error: "Bad Request" });
     });
 
-
     // update class status
     app.put("/update-status", async (req, res) => {
-        const { id, status } = req.query;
-        // Generate a new ObjectId
-        const objectId = new ObjectId(id);
-        const updatedClass = await classCollection.updateOne(
-            { _id: objectId },
-            { $set: { status } }
-        );
-        // console.log(updatedClass);
+      const { id, status } = req.query;
+      // Generate a new ObjectId
+      const objectId = new ObjectId(id);
+      const updatedClass = await classCollection.updateOne(
+        { _id: objectId },
+        { $set: { status } }
+      );
+      // console.log(updatedClass);
 
-        updatedClass.acknowledged
-            ? res.status(200).json({ message: "Status successfully updated" })
-            : res.status(400).json({ error: "Bad Request" });
-    }
-);
+      updatedClass.acknowledged
+        ? res.status(200).json({ message: "Status successfully updated" })
+        : res.status(400).json({ error: "Bad Request" });
+    });
 
-// send class approved/denied feedback
-app.put("/send-feedback", async (req, res) => {
-        const { id } = req.query;
-        // Generate a new ObjectId
-        const objectId = new ObjectId(id);
-        const updatedClass = await classCollection.updateOne(
-            { _id: objectId },
-            { $set: { feedback: req.body.message } },
-            { upsert: true }
-        );
-        // console.log(updatedClass);
+    // send class approved/denied feedback
+    app.put("/send-feedback", async (req, res) => {
+      const { id } = req.query;
+      // Generate a new ObjectId
+      const objectId = new ObjectId(id);
+      const updatedClass = await classCollection.updateOne(
+        { _id: objectId },
+        { $set: { feedback: req.body.message } },
+        { upsert: true }
+      );
+      // console.log(updatedClass);
 
-        updatedClass.acknowledged
-            ? res.status(200).json({ message: "Feedback successfully sent" })
-            : res.status(400).json({ error: "Bad Request" });
-});
-
+      updatedClass.acknowledged
+        ? res.status(200).json({ message: "Feedback successfully sent" })
+        : res.status(400).json({ error: "Bad Request" });
+    });
 
     // update user role
     app.put("/update-role", async (req, res) => {
-        const { role, email } = req.query;
+      const { role, email } = req.query;
 
-        // update the user
-        const updatedUser = await userCollection.updateOne(
-            { email: email },
-            { $set: { role } }
-        );
+      // update the user
+      const updatedUser = await userCollection.updateOne(
+        { email: email },
+        { $set: { role } }
+      );
 
-        updatedUser.acknowledged
-            ? res.status(200).json({ message: `Added as ${role}` })
-            : res.status(400).json({ error: "Bad Request" });
+      updatedUser.acknowledged
+        ? res.status(200).json({ message: `Added as ${role}` })
+        : res.status(400).json({ error: "Bad Request" });
     });
 
     // update selected class's seat
@@ -164,27 +160,26 @@ app.put("/send-feedback", async (req, res) => {
 
     // add a class as instructor
     app.post("/add-class", async (req, res) => {
-        const { name, email } = req.body;
-        console.log(req.body);
-        // find existing user
-        const existingClass = await classCollection.findOne({ name, email });
+      const { name, email } = req.body;
+      console.log(req.body);
+      // find existing user
+      const existingClass = await classCollection.findOne({ name, email });
 
-        if (existingClass) {
-            return res.json({ error: "Class already exist" });
-        }
+      if (existingClass) {
+        return res.json({ error: "Class already exist" });
+      }
 
-        const newClass = await classCollection.insertOne({
-            ...req.body,
-            status: "pending",
-            enrolledStudents: 0,
-        });
+      const newClass = await classCollection.insertOne({
+        ...req.body,
+        status: "pending",
+        enrolledStudents: 0,
+      });
 
-        // console.log(newUser);
-        newClass.acknowledged
-            ? res.status(200).json({ message: "Class successfully added" })
-            : res.status(400).json({ error: "Bad Request" });
-    }
-    )
+      // console.log(newUser);
+      newClass.acknowledged
+        ? res.status(200).json({ message: "Class successfully added" })
+        : res.status(400).json({ error: "Bad Request" });
+    });
 
     app.use((req, res, next) => {
       res.status(404).json({
